04b49d29f89935fe9aee893564d8d316e51c03ff
Muhammad Rifqi Zakariyah <41981740+RifqiZakariyah27@users.noreply.github.com>
2019-07-31 10:06:25 +0700

    Add files via upload

---
+++ CleanerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CleanerController extends Controller
+{
+    //
+    public function cleaner()
+    {
+        return view('tables');
+    }
+}
